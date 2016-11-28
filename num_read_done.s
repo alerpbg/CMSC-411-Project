86fc5773615b6b268cddcbe5a9caeef7f74cdc6e
Raphae Min <minraph1@umbc.edu>
2016-11-28 15:51:54 -0500

    Add files via upload
    Obtained and stored exponent part of number and placed into a buffer called "exponent".
    Obtained decimal part of number; Currently in r4 and r7 (why its in both is because MUL doesn't like when you multiply a register into itself)
    The number to be read in "readIn" must start with either a "+" or "-" AND end with a "$".

---
+++ num_read_done.s
@@ -0,0 +1,91 @@
+@ FIR Filter
+	.text
+	.global _start
+_start:
+	;load string
+	LDR r0,=readIn ;loads address of first index of string
+	LDRB r1, [r0]   ;loads first element of string into r1
+	LDR r8, [r0]		; get number of characters
+	
+	MOV r3, #43		; ASCII for "+"
+	;LDR r2,=pos    ;r2 points to positive sign
+	;LDRB r3, [r2]	;r3 is + sign
+	
+	;take string and load a negative sign as a 1
+	;take the parts before the decimal and convert those to binary for one value
+	;take the decimal and convert to ieee with whatever is after the decimal point
+	
+	MOV r5, #46		; ASCII for "."
+	;LDR  r4, =dot	; r4 points to "."
+	;LDRB r5, [r4]	; r5 is "."
+	MOV  r9, #10 	; r9 contains the constant 10
+	
+	;MOV r2, #0	;zero out r2 and r4 for later use
+	;MOV r4, #0
+
+	CMP r1, r5	;if sign is + then put a 0 in r6
+	BEQ positive
+
+negative:
+	
+	MOV r6,#1	;if sign is - then put 1in r6 for sign bit
+	MOV r1, #1	;set r1 to 1 (using r1 as string index offset)
+	B number_start
+
+positive:
+	
+	MOV r6,#0	;puts 0 in r6 for sign bit
+	MOV r1, #1	;set r1 to 1 (using r1 as string index offset)
+
+number_start:
+
+	LDRB r2, [r0, r1]; get character in number string
+	ADD r1, r1, #1	; r1 is string index offset
+	CMP r2, r5		; if character is "." then jump
+	BEQ decimal_start
+	
+	SUB r2, r2, #48		; ASCII -> actual number
+	
+	MUL r7, r4, r9 	; multiply by 10, basically (MUL r7, r7, #10) but syntax won't allow
+	ADD r7, r7, r2	; add the new digit
+	MOV r4, r7 		; r4 = r7 so we can mult by itself
+
+	B 	number_start
+
+decimal_start:
+	STR r4, =exponent
+	MOV r4, #0
+	MOV r7, #0
+
+decimal_loop:
+
+	LDRB r2, [r0, r1]; get character in number string
+	ADD r1, r1, #1	; r1 is string index offset
+	SUB r2, r2, #48		; ASCII -> actual number
+	MUL r7, r4, r9 	; multiply by 10, basically (MUL r7, r7, #10) but syntax won't allow
+	ADD r7, r7, r2	; add the new digit
+	MOV r4, r7 		; r4 = r7 so we can mult by itself
+	
+	ADD r2, r2, #48
+	CMP r2, #36		; if character is "$" then reached end of string
+	BEQ decimal_loop
+	
+	
+appendNums:
+	
+	
+
+appendFracNums:
+
+ 
+	;while not "." mov it into register 7
+	;while not end of string mov it into register 8 
+	
+
+	.data
+readIn: .asciz "-12.5$"
+exponent: .word 0
+;pos: 	.asciz "+"
+;dot:	.asciz "."
+
+.end
